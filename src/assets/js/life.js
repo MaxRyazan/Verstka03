d3f674a2b635330955a63f9f67c28b9bd8064104
MaxRyazan <no4ta87@mail.ru>
2023-02-21 20:22:36 +0300

    1.51 slider done

--- src/assets/js/life.js
+++ src/assets/js/life.js
@@ -1,31 +1,37 @@
 const maxCount = document.querySelector('.life__slider_inner').children.length
 const blockToMove = document.querySelector('.life__slider_inner')
-const sliderWidth = document.querySelector('.life__slider').clientWidth
-
+let sliderWidth = document.querySelector('.life__slider').clientWidth
 const buttonPrev = document.querySelector('.btn_prev')
 const buttonNext = document.querySelector('.btn_next')
-let count = 0
+let count = 0;
+
+window.addEventListener('resize', () => {
+    sliderWidth = document.querySelector('.life__slider').clientWidth
+})
 
 
-    buttonNext.addEventListener('click', () => {
-        if(count >= maxCount - 1){
-            count = -1
-        }
-        count++
-        blockToMove.style.transform = 'translate(-'+count*sliderWidth+'px, 0)';
-        console.log('next ', count)
-        if(count >= maxCount - 1){
-            count = -1
-            console.log('next in if ', count)
-        }
-    })
-    buttonPrev.addEventListener('click', () => {
-        count--
-        if(count < 0) {
-            count = maxCount - 1
-        }
-        blockToMove.style.transform = 'translate(-'+count*sliderWidth+'px, 0)';
-        console.log('prev ', count)
-    })
+buttonNext.addEventListener('click', () => {
+    if (count >= maxCount - 1) {
+        count = -1
+    }
+    count++
+    blockToMove.style.transform = 'translate(-' + count * sliderWidth + 'px, 0)';
+    blockToMove.style.transition = '.7s';
+    if (count >= maxCount - 1) {
+        count = -1
+    }
+})
+
+buttonPrev.addEventListener('click', () => {
+    if (count === -1) {
+        count = maxCount - 1
+    }
+    count--
+    if (count < 0) {
+        count = maxCount - 1
+    }
+    blockToMove.style.transform = 'translate(-' + count * sliderWidth + 'px, 0)';
+    blockToMove.style.transition = '.7s';
+})
 
 
