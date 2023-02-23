621c9781a74abbbae13a9e66a229c9d8a5bae8b5
MaxRyazan <no4ta87@mail.ru>
2023-02-23 19:16:39 +0300

    3

--- gulpfile.js
+++ gulpfile.js
@@ -102,6 +102,7 @@ function images(){
 
 function fonts(){
     return src(path.src.fonts, {base: srcPath + "assets/fonts/"})
+        .pipe(dest(path.build.fonts))
         .pipe(browserSync.reload({stream: true}))
 }
 
